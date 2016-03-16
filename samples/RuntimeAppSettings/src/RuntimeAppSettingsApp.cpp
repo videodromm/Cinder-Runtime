93d00c01acd3d02a2daa1996371fdd04fa1f72a0
simongeilfus <simongeilfus@gmail.com>
2016-03-16 13:22:02 +0100

    Added RuntimeAppSettings sample

---
+++ samples/RuntimeAppSettings/src/RuntimeAppSettingsApp.cpp
@@ -0,0 +1,70 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "CinderImGui.h"
+#include "Simplex.h"
+#include "runtime_app.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+struct Circle {
+	vec2 position;
+	float size;
+};
+
+class RuntimeAppSettingsApp : public App {
+  public:
+	void setup() override;
+	void draw() override;
+	
+	vector<Circle> mCircles;
+};
+
+void RuntimeAppSettingsApp::setup()
+{
+	ui::initialize();
+	
+	for( int i = 0; i < getWindowSize().x; i += 20 ) {
+		for( int j = 0; j < getWindowSize().y; j += 20 ) {
+			mCircles.push_back( { vec2( 10 ) + vec2( i, j ), Simplex::noise( vec2( i, j ) * 0.1f ) * 0.5f + 0.5f } );
+		}
+	}
+}
+
+void RuntimeAppSettingsApp::draw()
+{
+	gl::clear( Color( 1, 0, 0 ) );
+	
+	ui::ScopedWindow window( "RuntimeApp" );
+	ui::Text( "Hello!" );
+	
+	for( auto &circle : mCircles ) {
+		gl::drawSolidCircle( circle.position, circle.size * 10.0f );
+	}
+}
+
+CINDER_RUNTIME_APP( RuntimeAppSettingsApp, RendererGl, App::SettingsFn()
+#ifndef DISABLE_RUNTIME_COMPILED_APP
+	// The interpreter needs to know about the blocks we are using
+   ,[] ( cling::Interpreter *interpreter ) {
+	   auto appRoot = getAppPath().parent_path().parent_path().parent_path();
+	   auto blocksFolder = appRoot / "blocks";
+	   
+	   // Header-only blocks are easily added by just providing the include path
+	   interpreter->AddIncludePath( ( blocksFolder / "Simplex Noise/include" ).string() );
+	   
+	   // Some other blocks with .cpp files might be a bit more tricky especially when
+	   // there's some #defines that need to be processed before the rest like in Cinder-ImGui
+	   auto imguiFolder = blocksFolder / "ImGui";
+	   interpreter->AddIncludePath( ( imguiFolder / "lib/imgui" ).string() );
+	   interpreter->AddIncludePath( ( imguiFolder / "include" ).string() );
+	   interpreter->loadFile( ( imguiFolder / "include/CinderImGui.h" ).string(), false );
+	   interpreter->enableRawInput();
+	   interpreter->declare( ci::loadString( ci::loadFile( imguiFolder / "lib/imgui/imgui_draw.cpp" ) ) + "\n" + ci::loadString( ci::loadFile( imguiFolder / "lib/imgui/imgui.cpp" ) ) + "\n" + ci::loadString( ci::loadFile( imguiFolder / "src/CinderImGui.cpp" ) ) );
+	   interpreter->enableRawInput( false );
+   }
+#endif
+)
